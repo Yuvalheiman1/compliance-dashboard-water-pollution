486e75ecd5bb097d1494ad51d300b050762b7d91
Yuvalheiman1 <yuvalheiman1@gmail.com>
2024-11-27 15:59:55 +0000

    dataset fix

--- test_project/dataset.cpp
+++ test_project/dataset.cpp
@@ -1,80 +1,31 @@
-// COMP2811 Coursework 1 sample solution: QuakeDataset class
-
-#include <stdexcept>
-#include <algorithm>
-#include <numeric>
 #include "dataset.hpp"
 #include "csv.hpp"
+#include <stdexcept>
 
-using namespace std;
+void WaterDataset::loadData(const std::string& filename) {
+    csv::CSVReader reader(filename);
 
+    data.clear();
 
-void QuakeDataset::loadData(const string& filename)
-{
-  csv::CSVReader reader(filename);
-
-  data.clear();
-
-  for (const auto& row: reader) {
-    Quake quake{
-      row["time"].get<>(),
-      row["latitude"].get<double>(),
-      row["longitude"].get<double>(),
-      row["depth"].get<double>(),
-      row["mag"].get<double>()
-    };
-    data.push_back(quake);
-  }
+    for (const auto& row : reader) {
+        WaterSample sample{
+            row["sample.samplingPoint.label"].get<>(),
+            row["determinand.label"].get<>(),
+            row["result"].get<double>(),
+            row["determinand.unit.label"].get<>(),
+            row["sample.isComplianceSample"].get<bool>() ? "Compliant" : "Non-Compliant"
+        };
+        data.push_back(sample);
+    }
 }
 
-
-Quake QuakeDataset::strongest() const
-{
-  checkDataExists();
-
-  auto quake = max_element(data.begin(), data.end(),
-    [](Quake a, Quake b) { return a.getMagnitude() < b.getMagnitude(); });
-
-  return *quake;
+const std::vector<WaterSample>& WaterDataset::getData() const {
+    checkDataExists();
+    return data;
 }
 
-
-Quake QuakeDataset::shallowest() const
-{
-  checkDataExists();
-
-  auto quake = min_element(data.begin(), data.end(),
-    [](Quake a, Quake b) { return a.getDepth() < b.getDepth(); });
-
-  return *quake;
-}
-
-
-double QuakeDataset::meanDepth() const
-{
-  checkDataExists();
-
-  auto sum = accumulate(data.begin(), data.end(), 0.0,
-    [](double total, Quake q) { return total + q.getDepth(); });
-
-  return sum / data.size();
-}
-
-
-double QuakeDataset::meanMagnitude() const
-{
-  checkDataExists();
-
-  auto sum = accumulate(data.begin(), data.end(), 0.0,
-    [](double total, Quake q) { return total + q.getMagnitude(); });
-
-  return sum / data.size();
-}
-
-
-void QuakeDataset::checkDataExists() const
-{
-  if (size() == 0) {
-    throw std::runtime_error("Dataset is empty!");
-  }
+void WaterDataset::checkDataExists() const {
+    if (data.empty()) {
+        throw std::runtime_error("Dataset is empty or could not be loaded!");
+    }
 }
--- test_project/dataset.hpp
+++ test_project/dataset.hpp
@@ -1,24 +1,26 @@
-// COMP2811 Coursework 1 sample solution: QuakeDataset class
-
-#pragma once
+#ifndef WATERDATASET_HPP
+#define WATERDATASET_HPP
 
 #include <vector>
-#include "quake.hpp"
+#include <string>
+#include <map>
 
-class QuakeDataset
-{
-  public:
-    QuakeDataset() {}
-    QuakeDataset(const std::string& filename) { loadData(filename); }
-    void loadData(const std::string&);
-    int size() const { return data.size(); }
-    Quake operator[](int index) const { return data.at(index); }
-    Quake strongest() const;
-    Quake shallowest() const;
-    double meanDepth() const;
-    double meanMagnitude() const;
+struct WaterSample {
+    std::string location;
+    std::string pollutant;
+    double level;
+    std::string unit;
+    std::string complianceStatus;
+};
 
-  private:
-    std::vector<Quake> data;
+class WaterDataset {
+public:
+    void loadData(const std::string& filename);
+    const std::vector<WaterSample>& getData() const;
+
+private:
+    std::vector<WaterSample> data;
     void checkDataExists() const;
 };
+
+#endif // WATERDATASET_HPP
